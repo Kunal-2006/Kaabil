c87c58a6c39ca1fc34b2a3f9cf662439551f54dd
Kunal-2006 <me24i1022@iiitdm.ac.in>
2025-05-09 17:30:28 +0530

    Update code.py

--- Phase -1/TASK-2/code.py
+++ Phase -1/TASK-2/code.py
@@ -27,6 +27,31 @@ def main(args=None):
   rclpy.shutdown()"""Stops the ROS communication"""
 if __name__ =='__main__':
   main()
+#############################################################################################################
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+class MyNode(Node):
+
+    def __init__(self):
+        super().__init__("first_node")
+        self.counter_ = 0
+        self.create_timer(1.0, self.timer_callback) """created a timer by which after 1 second it will print Hello"""
+
+    def timer_callback(self):
+        self.get_logger().info("Hello " + str(self.counter_)) """ print Hello and the nummber of the times hello printed with the help of concatanation.
+        self.counter_ += 1
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
 
 
   
